3b9bc1a21612cdccf62bb4f3f9b87fd6b9a4ae52
rod martin <rod@araknia.com>
2020-07-26 19:20:29 +0100

    debug out put

---
+++ Assets/Scripts/DebugCanvaser.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugCanvaser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform watchCam;
+    private Text debugText;
+    public bool debugMessages = true;
+    void Start()
+    {
+        watchCam = Camera.current.transform;
+        debugText = GetComponent<Text>();
+        if (debugMessages)
+        {
+            if (debugText == null)
+            {
+                debugMessages = false;
+                debugText.text = "No Text Canvas";
+            }
+            else
+            {
+                debugText.text = "Start Debug";
+            }
+            if (watchCam == null)
+            {
+                debugMessages = false;
+                debugText.text = "No Camera";
+            }
+           
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        debugText.text = "X: " + watchCam.position.x + " Z: " + watchCam.position.z;
+    }
+}
