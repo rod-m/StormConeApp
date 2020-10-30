48e54cef8b7a8ea6849ab3915e5e0f97b81d3e01
NachoPepe <i.j.rocamascaros@edu.salford.ac.uk>
2020-10-30 03:32:54 +0000

    Sound Stuff, Gps stuff, AR stuff

--- Assets/Scripts/GPSScripts/TestLocationService.cs
+++ Assets/Scripts/GPSScripts/TestLocationService.cs
@@ -4,29 +4,24 @@ using UnityEngine;
 using UnityEngine.Android;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Canvasmanager))]
 public class TestLocationService : MonoBehaviour
 {
-
+    [SerializeField]
+    Text m_LocationText;
     [SerializeField]
     Button m_StartGPSButton;
-    [SerializeField]
-    Button m_PlaceAgainButton;
 
 
-    private Canvasmanager canvasmanager;
-
-    private float GPSX;
-    private float GPSZ;
+    public float GPSX;
+    public float GPSZ;
 
     public bool test;
     public GameObject player;
-    public GameObject soundSource;
-    private List<GameObject> placeObjects = new List<GameObject>();
+
     private void Start()
     {
-        canvasmanager = GetComponent<Canvasmanager>();
-
+        GPSX = 0;
+        GPSZ = 0;
     }
 
 
@@ -40,40 +35,51 @@ public class TestLocationService : MonoBehaviour
         if (m_StartGPSButton != null)
             m_StartGPSButton.gameObject.SetActive(active);
     }
-    
-    void SetPlaceAgainActive(bool active)
+    public Text locationText
     {
-        if (m_PlaceAgainButton != null)
-            m_PlaceAgainButton.gameObject.SetActive(active);
+        get { return m_LocationText; }
+        set { m_LocationText = value; }
     }
 
-    
+    [SerializeField]
+    Text m_LogText;
+
+    public Text logText
+    {
+        get { return m_LogText; }
+        set { m_LogText = value; }
+    }
+
+    void Log(string message)
+    {
+        m_LogText.text += $"{message}\n";
+    }
 
     IEnumerator CheckGPSSupport()
     {
-        canvasmanager.m_LogText.text = "Init GPS!";
+        m_LogText.text = "Starting GPS";
         // First, check if user has location service enabled
         if (!Input.location.isEnabledByUser)
         {
-        
-            canvasmanager.Log("*** GPS Not Enabled By USER! ***");
+            m_LogText.text = "*** GPS Not Enabled By USER! ***";
+
             yield return new WaitForSeconds(3);
-            canvasmanager.m_LocationText.text = "NO GPS";
-            canvasmanager.m_LogText.text = "";
+            m_LocationText.text = "NO GPS";
+            m_LogText.text = "";
 
             yield break;
         }
 
 
         // Start service before querying location
-        Input.location.Start(2, 2);
+        Input.location.Start(1, 1);
 
         // Wait until service initializes
         int maxWait = 20;
         while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
         {
-            //Debug.Log("Waiting " + maxWait);
-            canvasmanager.m_LocationText.text = "Waiting... " + maxWait;
+            Debug.Log("Waiting " + maxWait);
+            m_LocationText.text = "Waiting... " + maxWait;
             yield return new WaitForSeconds(1);
             maxWait--;
         }
@@ -81,76 +87,51 @@ public class TestLocationService : MonoBehaviour
         // Service didn't initialize in 20 seconds
         if (maxWait < 1)
         {
-            canvasmanager.Log("Timed out");
-            canvasmanager.m_LocationText.text = "GSP Timeout 20s!";
+            Log("\nTimed out");
+            m_LocationText.text = "GSP Timeout 20s!";
             yield break;
         }
 
         // Connection has failed
         if (Input.location.status == LocationServiceStatus.Failed)
         {
-            canvasmanager.Log("Unable to determine device location");
-            canvasmanager.m_LocationText.text = "Unable to determine device location";
+            Log("\nUnable to determine device location");
+            m_LocationText.text = "Unable to determine device location";
             yield break;
         }
         else
         {
             // Access granted and location value could be retrieved
-            canvasmanager.Log( Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+            Log("\nLocation: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
         }
 
         // Stop service if there is no need to query location updates continuously
 
         // Input.location.Stop();
-        Input.compass.enabled = true;
         StartCoroutine(CheckGPS());
     }
 
-    public void PlaceAgain()
-    {
-        test = true;
-    }
     IEnumerator CheckGPS()
     {
         while (true)
         {
             if (test)
             {
-                if (placeObjects.Count > 0)
-                {
-                    foreach (var gm in placeObjects)
-                    {
-                        Destroy(gm);
-                    }
-                    placeObjects.Clear();
-                }
-                SetPlaceAgainActive(false);
-                canvasmanager.m_LocationText.text = "Adding Location Marker";
-                yield return new WaitForSeconds(3f);
-                var g = Instantiate(soundSource);
-                g.name = "sound source";
-                placeObjects.Add(g);
-                yield return new WaitForSeconds(0.1f);
-                GameObject p = Instantiate(player);
-                placeObjects.Add(p);
+                //Instantiate(player);
+                player.GetComponent<GPS_Move>().Activate();
                 test = false;
-                canvasmanager.m_LocationText.text = $"Added Location Marker {Input.location.lastData.latitude} {Input.location.lastData.longitude}";
-                                      
-                yield return new WaitForSeconds(2f);
-                SetPlaceAgainActive(true);
             }
             GPSX = Input.location.lastData.latitude;
             GPSZ = Input.location.lastData.longitude;
-            canvasmanager.m_LocationText.text = $"Lon: {GPSZ} Lat: {GPSX}\n Accuracy: {Input.location.lastData.horizontalAccuracy}m";
-                                 
+            m_LocationText.text = "Location: " + Input.location.lastData.latitude + " " +
+                                  Input.location.lastData.longitude;
             //              " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy;
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(1);
         }
     }
 
     public void StartGPS()
     {
-        // allow me to place again without force close app
         SetStartGPSButtonActive(false);
 
         StartCoroutine(CheckGPSSupport());
@@ -168,7 +149,7 @@ public class TestLocationService : MonoBehaviour
 
     void OnEnable()
     {
-        SetPlaceAgainActive(false);
+
         if (Permission.HasUserAuthorizedPermission(Permission.FineLocation))
         {
             // The user authorized use of the FineLocation.
@@ -179,7 +160,6 @@ public class TestLocationService : MonoBehaviour
             // Ask for permission or proceed without the functionality enabled.
             Permission.RequestUserPermission(Permission.FineLocation);
         }
-        
         //
 
     }
---
+++ Assets/Scripts/GPS_Move.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GPS_Move : MonoBehaviour
+{
+    public double targetLocationLon;
+    public double targetLocationLat;
+
+    public TestLocationService tS;
+    public Text oD;
+
+    public bool mercProjection;
+    private bool engaged;
+
+    private double gpsX;
+    private double gpsZ;
+    private double offsetX;
+    private double offsetZ;
+    private double ppX;
+    private double ppZ;
+    private double x;
+    private double z;
+
+    public Text xT;
+    public Text yT;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        engaged = false;
+        //xT = GameObject.FindGameObjectWithTag("Ppx").GetComponent<Text>();
+        //yT = GameObject.FindGameObjectWithTag("Ppy").GetComponent<Text>();
+        //tS = GameObject.FindGameObjectWithTag("GameController").GetComponent<TestLocationService>();
+        //oD = GameObject.FindGameObjectWithTag("Finish").GetComponent<Text>();
+    }
+
+    public void Activate()
+    {
+        
+        StartCoroutine(setOffset());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (engaged) 
+        {
+            StartCoroutine(updateGPS());
+            engaged = false;
+        }
+    }
+
+    private void updatePos() 
+    {
+        gpsX -= offsetX;
+        gpsZ -= offsetZ;
+        if (mercProjection)
+        {
+            x = MapTools.MercatorProjection.lonToX(gpsX);
+            z = MapTools.MercatorProjection.latToY(gpsZ);
+        }
+        else 
+        {
+            x = ConvertGPSCoords(gpsX);
+            z = ConvertGPSCoords(gpsZ);
+        }
+        
+        transform.position = new Vector3((float)x, 0, (float)z);
+        float f = Vector2.Distance(new Vector2((float)x, (float)z), new Vector2(0, 30));
+        oD.text = f.ToString();
+    }
+
+    private double ConvertGPSCoords(double n) 
+    {
+        return n*(111190f);
+    }
+
+    private IEnumerator setOffset() 
+    {
+        yield return new WaitForSeconds(3);
+        offsetX = tS.GPSX;
+        offsetZ = tS.GPSZ;
+        ppX = MapTools.MercatorProjection.lonToX(offsetX - targetLocationLon);
+        ppZ = MapTools.MercatorProjection.latToY(offsetZ - targetLocationLat);
+        xT.text = ppX.ToString();
+        yT.text = ppZ.ToString();
+        engaged = true;
+    }
+
+    private void updateObjDistance() {
+        ppX = MapTools.MercatorProjection.lonToX(gpsX - targetLocationLon);
+        ppZ = MapTools.MercatorProjection.latToY(gpsZ - targetLocationLat);
+    }
+
+    private IEnumerator updateGPS() 
+    {
+        yield return new WaitForSeconds(1);
+        gpsX = tS.GPSX;
+        gpsZ = tS.GPSZ;
+        updatePos();
+        updateObjDistance();
+        StartCoroutine(updateGPS());
+    }
+}
