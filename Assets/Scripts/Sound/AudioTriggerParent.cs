b2307f4c25706c9decbf3eedb4b2fc47bcc1789c
Nela <ciresika99@yahoo.com>
2020-10-30 13:55:08 +0000

    Revert "Revert "Sound Stuff, Gps stuff, AR stuff""
    This reverts commit 60ed8afe4e3c69bed695931c60d3fc3bca2beb76.

---
+++ Assets/Scripts/Sound/AudioTriggerParent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTriggerParent : MonoBehaviour
+{
+
+    private AudioSource aS;
+    private AudioCoordinator aC;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        aS = GetComponentInParent<AudioSource>();
+        aC = GameObject.FindGameObjectWithTag("Player").GetComponent<AudioCoordinator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            Debug.Log("Enter");
+            //aS.Play();
+            aC.playOnTime(aS);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Exit");
+            aS.Stop();
+        }
+    }
+}
